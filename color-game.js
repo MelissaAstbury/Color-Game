2a9812e01bd07bf9db35af82b421511b1ed28e21
Melissa Astbury <melissajastbury@gmail.com>
2020-02-15 21:40:41 +0000

    adding event listener

--- color-game.js
+++ color-game.js
@@ -15,5 +15,20 @@ let squares = document.querySelectorAll(".square");
 
 for (let i = 0; i < squares.length; i++) {
     squares[i].style.backgroundColor = colors[i];
-};
+    //Add color number to span in HTML 
+    let pickedColor = colors[3];
+    let colorDisplay = document.getElementById("colorDisplay");
 
+    colorDisplay.textContent = pickedColor;
+    //add click listeners to the squares
+
+    squares[i].addEventListener("click", (e) => {
+        let colourClicked = e.toElement.style.backgroundColor;
+        //compare the pickedColor to the clickedColour
+        if (colourClicked === pickedColor) {
+            alert('You are right');
+        } else {
+            alert("You are wrong");
+        }
+    });
+};
